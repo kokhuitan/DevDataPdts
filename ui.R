6b5172bbc54233b3e4b1224e5e82bbb39da109de
kokhuitan <tan_kok_hui@rp.edu.sg>
2015-04-26 13:03:17 +0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,11 +1,10 @@
 library(shiny)
 shinyUI(pageWithSidebar(
-  headerPanel("BMI calculator"),
+  headerPanel("Body Mass Index (BMI) calculator"),
   sidebarPanel(
     h3('Enter your information below'),
     numericInput("weight", "Weigth (kg)", value = 60, min = 1, max = 1000, step=1),
     numericInput("height", "heigth (cm)", value = 170, min = 30, max = 300, step=1),
-    #radioButtons("sex", "Sex", c("Male","Female"), "Male"),
     submitButton("Submit")
   ),
   mainPanel(
@@ -13,6 +12,9 @@ shinyUI(pageWithSidebar(
     h4("BMI"),
     verbatimTextOutput("bmi"),
     h4("Status"),
-    verbatimTextOutput("status")
+    verbatimTextOutput("status"),
+    h5("BMI is an indicator of general health based solely on 
+       weight and height. For more detailed examination, please 
+       consult your general practitioner.")
   )
 ))
